7f8bb95d08b28494abdfd6bb0a0a32ee9dab7867
KelseaH <theofficialkelsea@gmail.com>
2023-07-03 14:16:04 -0700

    Small fixes

--- script.js
+++ script.js
@@ -13,19 +13,15 @@ function makePiece(team, id) {
 }
 
 board = [
-    [null, makePiece(1, 1), null, makePiece(0, 2), null, makePiece(0, 3), null, makePiece(0, 7)],
-    [makePiece(1, 0), null, makePiece(1, 2), null, makePiece(1, 4), null, makePiece(1, 6), null],
-    [null, makePiece(2, 1), null, makePiece(2, 3), null, makePiece(2, 5), null, makePiece(2, 7)],
-    [makePiece(3, 0), null, makePiece(3, 2), null, makePiece(3, 4), null, makePiece(3, 6), null],
-    [null, makePiece(4, 1), null, makePiece(4, 3), null, makePiece(4, 5), null, null],
-    [makePiece(5, 0), null, null, null, null, null, null, null],
-    [null, makePiece(6, 1), null, null, null, null, null, null],
-    [makePiece(7, 0), null, null, null, null, null, null, null],
+    [null, makePiece(0, 1), null, makePiece(0, 2), null, makePiece(0, 3), null, makePiece(0, 4)],
+    [makePiece(0, 5), null, makePiece(0, 6), null, makePiece(0, 7), null, makePiece(0, 8), null],
+    [null, makePiece(0, 9), null, makePiece(0, 10), null, makePiece(0, 11), null, makePiece(0, 12)],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [makePiece(1, 1), null, makePiece(1, 2), null, makePiece(1, 3), null, makePiece(1, 4), null],
+    [null, makePiece(1, 5), null, makePiece(1, 6), null, makePiece(1, 7), null, makePiece(1, 8)],
+    [makePiece(1, 9), null, makePiece(1, 10), null, makePiece(1, 11), null, makePiece(1, 12), null],
 ]
-/*
-function splitId(str_id) {
-    return [Number(str_id[0]), Number(str_id[2])];
-}*/
 
 dragObject = null;
 
@@ -46,25 +42,25 @@ function stopDrag(event) {
     event.target.appendChild(dragObject)
 }
 
-goldpeice = document.getElementsByClassName("goldPiece");
-console.log(goldpeice);
+goldpiece = document.getElementsByClassName("goldPiece");
+console.log(goldpiece);
 
-goldpeice.ondragstart = ondrag;
+goldpiece.ondragstart = ondrag;
 
-for (var i=0; i < 1; i++) {
+for (var i=0; i < 8; i++) {
     for (var j=0; j < 8; j++) {
         if (board[i][j] != null) {
-            let peice = board[i][j];
+            let piece = board[i][j];
             let element = document.createElement("div");
-            if (peice.team == 0) {
+            if (piece.team == 0) {
                 element.classList.add("goldPiece");
             } else {
                 element.classList.add("blackPiece");
             }
             element.setAttribute("draggable", true);
             element.ondragstart = startDrag;
-            element.textContent = String(peice.id);
-            boardSpace = document.getElementById(String(j) + "_" + String(i));
+            element.textContent = String(piece.id);
+            boardSpace = document.getElementById(String(i) + "_" + String(j));
             boardSpace.appendChild(element);
         }
     }
